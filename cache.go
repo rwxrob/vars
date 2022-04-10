258e11d136598f0ff78d2bddc5c85f38494bb6e0
rwxrob <rob@rwx.gg>
2022-04-10 17:12:31 -0400

    Update version

--- cache.go
+++ cache.go
@@ -30,7 +30,7 @@ var Cmd = &Z.Cmd{
 
 	Name:      `var`,
 	Summary:   `cache variables in {{ execachedir "vars"}}`,
-	Version:   `v0.0.1`,
+	Version:   `v0.1.0`,
 	Copyright: `Copyright 2021 Robert S Muhlestein`,
 	License:   `Apache-2.0`,
 	Commands:  []*Z.Cmd{help.Cmd, _init, set, get, _file, data, edit},
