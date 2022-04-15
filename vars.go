d364d188b9a4e0b9becf3926117e60a871725f26
rwxrob <rob@rwx.gg>
2022-04-15 10:42:08 -0400

    Update deps

--- vars.go
+++ vars.go
@@ -30,7 +30,7 @@ var Cmd = &Z.Cmd{
 
 	Name:      `var`,
 	Summary:   `cache variables in {{ execachedir "vars"}}`,
-	Version:   `v0.2.7`,
+	Version:   `v0.2.8`,
 	Copyright: `Copyright 2021 Robert S Muhlestein`,
 	License:   `Apache-2.0`,
 	Commands:  []*Z.Cmd{help.Cmd, _init, set, get, _file, data, edit},
